36f72ea1725d1649e5fa630da5776c2445bda490
topdev1030 <mercury427427@gmail.com>
2023-01-10 07:56:02 +0800

    ........

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// css
 import '../styles/globals.css'
 
+// utils
+import theme from '../utils/theme';
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Netflix</title>
+        <meta name="description" content="Netflix landing page" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
---
+++ utils/theme.js
@@ -0,0 +1,18 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { red, grey } from '@mui/material/colors';
+
+
+const base = createTheme({
+    palette: {
+        primary: {
+            main: red[600],
+        },
+        secondary: {
+            main: grey[900],
+        },
+    },
+});
+
+const theme = responsiveFontSizes(base)
+
+export default theme;
